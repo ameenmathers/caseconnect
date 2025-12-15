8cfdd0c4288d88173611577fa36acdf38faf101e
ameenmathers <alameenidris710@gmail.com>
2025-12-15 21:15:25 +0100

    fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,13 +27,20 @@ class AppServiceProvider extends ServiceProvider
     {
         $storagePath = '/tmp/storage';
 
-        if (!is_dir($storagePath)) {
-            mkdir($storagePath, 0755, true);
-            mkdir($storagePath . '/framework/views', 0755, true);
-            mkdir($storagePath . '/framework/cache', 0755, true);
-            mkdir($storagePath . '/framework/sessions', 0755, true);
-            mkdir($storagePath . '/logs', 0755, true);
-            mkdir($storagePath . '/app', 0755, true);
+        $directories = [
+            $storagePath,
+            $storagePath . '/framework',
+            $storagePath . '/framework/views',
+            $storagePath . '/framework/cache',
+            $storagePath . '/framework/sessions',
+            $storagePath . '/logs',
+            $storagePath . '/app',
+        ];
+
+        foreach ($directories as $directory) {
+            if (!is_dir($directory)) {
+                mkdir($directory, 0755, true);
+            }
         }
 
         $this->app->useStoragePath($storagePath);
