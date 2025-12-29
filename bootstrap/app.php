8177c2c808353ef3cd3a3f765b9804326b245504
ameenmathers <alameenidris710@gmail.com>
2025-12-29 18:20:42 +0100

    fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,6 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
+            \App\Http\Middleware\ForceJsonResponse::class,
             \Illuminate\Http\Middleware\HandleCors::class,
         ]);
     })
