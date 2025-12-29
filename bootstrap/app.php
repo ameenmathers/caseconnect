b9e193a6bf9d578f5aa5b9f2b1abf93c3a09d7a4
ameenmathers <alameenidris710@gmail.com>
2025-12-29 18:19:08 +0100

    fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
