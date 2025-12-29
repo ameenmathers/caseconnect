77a478f6258477ad691fd6dfe49ac7456d27c1f2
ameenmathers <alameenidris710@gmail.com>
2025-12-29 18:23:05 +0100

    fixes

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -8,12 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ForceJsonResponse
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->is('api/*')) {
-            $request->headers->set('Accept', 'application/json');
-        }
-
         return $next($request);
     }
 }
